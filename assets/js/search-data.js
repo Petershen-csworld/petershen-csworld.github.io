05472c7593ab9d54a16c291adf182f7793703fe9
Petershen-csworld <117569172+Petershen-csworld@users.noreply.github.com>
2026-04-12 13:32:18 +0000

    Deploying to gh-pages from @ Petershen-csworld/petershen-csworld.github.io@2e20641a0cbf7245ae6c39d69b8044d6e6207da1 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -72,7 +72,18 @@ ninja.data = [{
               handler: () => {
                 window.location.href = "/blog/";
               },
-            },{id: "post-a-post-with-plotly-js",
+            },{id: "post-hongkong",
+        
+          title: "Hongkong",
+        
+        description: "",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2026/hongkong/";
+          
+        },
+      },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
         
